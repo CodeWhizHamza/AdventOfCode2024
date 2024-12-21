a761a3a02afe50540cd26167f648530d8f23e34d
Hamza <muhammadhamzam1486@gmail.com>
2024-12-21 17:20:54 +0500

    Implement word counting functionality with regex for test lines in day 4

--- day4/main.go
+++ day4/main.go
@@ -2,10 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 )
 
 func main() {
-	lines := getLines()
-	charactersMatrix := getCharactersMatrix(lines)
-	fmt.Println(charactersMatrix)
+	// lines := getLines()
+	// charactersMatrix := getCharactersMatrix(lines)
+
+	testLines := []string{
+		"MMMSXXMASM",
+		"MSAMXMSMSA",
+		"AMXSXMAAMM",
+		"MSAMASMSMX",
+		"XMASAMXAMM",
+		"XXAMMXXAMA",
+		"SMSMSASXSS",
+		"SAXAMASAAA",
+		"MAMMMXMMMM",
+		"MXMXAXMASX",
+	}
+
+	forwardAndBackwardCount := countForwardAndBackwardWords(testLines)
+	fmt.Println(forwardAndBackwardCount)
+}
+
+func countForwardAndBackwardWords(lines []string) int {
+	forwardRegex, err := regexp.Compile(`XMAS`)
+	if err != nil {
+		log.Fatal("Cannot compile regex")
+	}
+	backwardRegex, err := regexp.Compile("SAMX")
+	if err != nil {
+		log.Fatal("Cannot compile regex")
+	}
+
+	count := 0
+
+	for _, line := range lines {
+		count += len(forwardRegex.FindAllString(line, -1))
+		count += len(backwardRegex.FindAllString(line, -1))
+	}
+
+	return count
 }
